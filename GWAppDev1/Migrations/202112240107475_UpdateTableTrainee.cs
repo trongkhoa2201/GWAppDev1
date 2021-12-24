c343357f7407f35242e997d9444bff46538ea6cd
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2021-12-24 08:12:40 +0700

    updatetrainee

---
+++ GWAppDev1/Migrations/202112240107475_UpdateTableTrainee.cs
@@ -0,0 +1,34 @@
+﻿namespace GWAppDev1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateTableTrainee : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CourseTrainees",
+                c => new
+                    {
+                        CourseId = c.Int(nullable: false),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.CourseId, t.UserId })
+                .ForeignKey("dbo.Courses", t => t.CourseId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.CourseId)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CourseTrainees", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.CourseTrainees", "CourseId", "dbo.Courses");
+            DropIndex("dbo.CourseTrainees", new[] { "UserId" });
+            DropIndex("dbo.CourseTrainees", new[] { "CourseId" });
+            DropTable("dbo.CourseTrainees");
+        }
+    }
+}
